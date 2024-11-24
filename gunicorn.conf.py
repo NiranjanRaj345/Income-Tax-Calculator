474a8c264cbe100b65dcb59cdfcf3f66350143fe
Niranjan Rajkumar <niranrnk003@gmail.com>
2024-11-25 01:21:10 +0530

    render deploy update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,18 @@
-workers = 4
+import multiprocessing
+
+# Worker configuration
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = 2
+worker_class = 'sync'
+
+# Binding
 bind = "0.0.0.0:10000"
+
+# Timeout configuration
 timeout = 120
+keepalive = 5
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
